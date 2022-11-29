7a09c602d5ae706820369c84745df25cf33b4903
omerfarukaydin61 <101247828+omerfarukaydin61@users.noreply.github.com>
2022-11-30 01:15:10 +0300

    Add files via upload

---
+++ YZV201_hw1/main/WordList.cpp
@@ -0,0 +1,150 @@
+﻿//--- 2022-2023 Fall YZV201E Assignment 1 ---//
+//--------------------------//
+//---Name & Surname: Omer Faruk Aydın
+//---Student Number: 150210726
+//--------------------------//
+
+//-------------Do Not Add New Libraries-------------//
+//-------------All Libraries Needed Were Given-------------//
+
+#include <iostream>  // For input/output operations
+#include <cstring>
+#include <vector>
+using namespace std;
+#include "WordList.h"
+
+//-------------Word Methods-------------//
+
+Word::Word() {
+	word_ = "";
+	edit_dist_ = 999;  // An arbitrary number to set the distance as infinite
+	next_ = NULL;
+}
+
+Word::Word(std::string vocab_word) {
+	word_ = vocab_word;
+	edit_dist_ = 999;  // An arbitrary number to set the distance as infinite
+	next_ = NULL;
+}
+
+int Word::EditDist(std::string str) {
+	return this->CalculateLevenshteinDistance(str, this->word_);
+}
+
+/* Levenshtein Distance Calculator */
+int Word::CalculateLevenshteinDistance(std::string target, std::string source) {
+	int targetLength = target.size();
+	int sourceLength = source.size();
+	int** distance = new int* [targetLength + 1];
+	for (int i = 0; i < targetLength + 1; i++) {
+		distance[i] = new int[sourceLength + 1];
+	}
+
+	for (int i = 0; i <= targetLength; i++) {
+		distance[i][0] = i;
+	}
+	for (int j = 0; j <= sourceLength; j++) {
+		distance[0][j] = j;
+	}
+	for (int i = 1; i <= targetLength; i++) {
+		for (int j = 1; j <= sourceLength; j++) {
+			int cost = Word::SubsCost(target[i - 1], source[j - 1]);
+			distance[i][j] = Word::Min(Word::Min(distance[i - 1][j] + 1, distance[i][j - 1] + 1), distance[i - 1][j - 1] + cost);
+		}
+	}
+	return distance[targetLength][sourceLength];
+}
+int Word::Min(int a, int b) {
+	return a < b ? a : b;
+}
+int Word::Min(int a, int b, int c) {
+	return Min(a, Min(b, c));
+}
+int Word::SubsCost(char c1, char c2) {
+	return c1 == c2 ? 0 : 2;
+}
+
+//-------------WordList Methods-------------//
+
+WordList::WordList() {
+	this->head_ = NULL;
+}
+
+WordList::~WordList() {
+	Word* traverse = head_;
+
+	// To make sure that we clear the memory when the program ends.
+	while (traverse != NULL) {
+		Word* temp = traverse;
+		traverse = traverse->next_;
+		delete temp;
+	}
+}
+
+void WordList::AddWord(std::string str) {
+
+	Word* newWord = new Word(str);
+
+	if (IsEmpty())
+	{
+		this->head_ = newWord;
+		return;
+	}
+
+	Word* tempWord = this->head_;
+
+	while (tempWord->next_ != NULL)
+	{
+		tempWord = tempWord->next_;
+	}
+	tempWord->next_ = newWord;
+}
+
+void WordList::PrintWords(int count) {
+
+	if (!(count > 0) || IsEmpty())
+		return;
+
+	Word* tempWord = this->head_;
+
+	while (count > 0 && tempWord != NULL)
+	{
+		PrintToConsole(tempWord->word_);
+		tempWord = tempWord->next_;
+		count--;
+	}
+}
+
+void WordList::CalcEditDists(std::string str) {
+	if (IsEmpty())
+		return;
+
+	Word* tempWord = this->head_;
+
+	while (tempWord != NULL)
+	{
+		tempWord->edit_dist_ = tempWord->EditDist(str);
+		tempWord = tempWord->next_;
+	}
+}
+
+void WordList::PrintEditDists(int count) {
+	if (!(count > 0) || IsEmpty())
+		return;
+
+	Word* tempWord = this->head_;
+
+	while (count > 0 && tempWord != NULL)
+	{
+		PrintToConsole(std::to_string(tempWord->edit_dist_));
+		tempWord = tempWord->next_;
+		count--;
+	}
+}
+void WordList::PrintToConsole(std::string str) {
+	std::cout << str << std::endl;
+}
+
+bool WordList::IsEmpty() {
+	return this->head_ == NULL;
+}
---
+++ YZV201_hw1/main/WordList.h
@@ -0,0 +1,58 @@
+//--- 2022-2023 Fall YZV201E Assignment 1 ---//
+//--------------------------//
+//---Name & Surname: Ömer Faruk AYDIN
+//---Student Number: 150210726
+//--------------------------//
+
+//-------------Do Not Add New Libraries-------------//
+//-------------All Libraries Needed Were Given-------------//
+
+#include <string>  // For string operations
+
+
+// Class for nodes of the list
+class Word {
+public:
+	std::string word_;  // Variable to keep the word
+	int edit_dist_;  // Variable to keep the edit distance of that word
+	Word* next_;  // Pointer to keep the address of the next node
+
+	// Calculate substituion cost. Return 0 if two letters are same,
+	// return 2 otherwise.
+	int SubsCost(char, char);
+	// Calculate edit distance for a node.
+	int EditDist(std::string);
+
+	Word();  // Default consturactor
+	Word(std::string);  // Constructor for the given word
+private:
+	int CalculateLevenshteinDistance(std::string, std::string);
+	int Min(int, int);
+	int Min(int, int, int);
+};
+
+// Class for the list
+class WordList {
+public:
+	Word* head_;  // Pointer that points to the first node of the list
+	Word* tail_;  // Pointer that points to the last node of the list
+
+	// Add all words to the list
+	void AddWord(std::string);
+	// Print first "n" words from the list
+	void PrintWords(int);
+	// Calculate edit distances for all words in the list
+	void CalcEditDists(std::string);
+	// Print first "n" words' edit distance from the list
+	void PrintEditDists(int);
+	// Print "n" words that has the lowest edit distance
+
+	WordList();  // Default consturactor
+	~WordList();  // Destructor
+
+private:
+	// Returns bool about list empty or not
+	bool IsEmpty();
+	// Prints strings to console
+	void PrintToConsole(std::string);
+};
